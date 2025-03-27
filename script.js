1d9f116376b0b46b2a30b8cbbbb81dc0b20cb512
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-27 21:05:03 +0300

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+//Event Listeners
 document.getElementById('randomPaletteBtn').addEventListener('click', generateRandomPalette);
 document.getElementById('baseColorBtn').addEventListener('click', generateFromBaseColor);
 document.getElementById('imageColorBtn').addEventListener('click', extractColorsFromImage);
 
+//Random Palette Generator
 async function generateRandomPalette() {
     const response = await fetch('https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome&count=5');
     const data = await response.json();
     displayPalette(data.colors.map(color => color.hex.value));
 }
 
+//Base Colour Palette Generator
 function generateFromBaseColor() {
     const baseColor = document.getElementById('baseColor').value.substring(1);
     fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=analogic&count=5`)
@@ -15,6 +18,7 @@ function generateFromBaseColor() {
         .then(data => displayPalette(data.colors.map(color => color.hex.value)));
 }
 
+// Display Palette
 function displayPalette(colors) {
     const palette = document.getElementById('palette');
     palette.innerHTML = '';
