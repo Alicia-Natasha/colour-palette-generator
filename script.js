115c12e20dacab09f2072342554d2018a5342472
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-03-27 20:42:57 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.getElementById('randomPaletteBtn').addEventListener('click', generateRandomPalette);
 document.getElementById('baseColorBtn').addEventListener('click', generateFromBaseColor);
 document.getElementById('imageColorBtn').addEventListener('click', extractColorsFromImage);
+
+async function generateRandomPalette() {
+    const response = await fetch('https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome&count=5');
+    const data = await response.json();
+    displayPalette(data.colors.map(color => color.hex.value));
+}
